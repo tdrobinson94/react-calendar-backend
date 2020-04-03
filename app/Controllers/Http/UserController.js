f59aaa57e604fb249b7ca6824fb1ecddfb50fd46
Terry Robinson <tdrobinson94@comcast.net>
2020-04-03 00:01:04 -0400

    updated

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -82,6 +82,8 @@ class UserController {
 
   async destroy ({ request, response, params: { id } }) {
     // const user = request.post().user
+    const input = request.only('username', 'password')
+    input.id = request.authUser.id;
     const user = User.findBy('id', request.param('id'))
 
     await user.delete()
