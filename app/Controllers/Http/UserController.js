3eea4df45de66a9bedd39c1cb8e129fb6010b991
Terry Robinson <tdrobinson94@comcast.net>
2020-04-03 13:00:42 -0400

    updated

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -57,7 +57,7 @@ class UserController {
 
 
   async show({ auth, request, response, params: { id } }) {
-    const user = auth.user.id
+    const user = await User.find(id)
 
     return response.status(200).json({
       message: 'Here is your user',
