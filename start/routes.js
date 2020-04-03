3fd0fa50a09f2f8c70a129bb1d99d7c6e470cbc8
Terry Robinson <tdrobinson94@comcast.net>
2020-04-03 13:19:01 -0400

    updated

--- start/routes.js
+++ start/routes.js
@@ -21,7 +21,7 @@ Route.get('/', () => {
 })
 Route.post('/signup', 'UserController.store')
 Route.get('/users', 'UserController.index')
-Route.get('/user/:id', 'UserController.show').middleware(['FindUser'])
+Route.get('/user/:id', 'UserController.show').middleware(['auth:jwt'])
 Route.patch('/updateuser/:id', 'UserController.update').middleware(['auth:jwt'])
 Route.delete('/deleteuser/:id', 'UserController.destroy').middleware(['auth:jwt'])
 
