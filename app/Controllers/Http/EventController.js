ff7712b662ff7d860b4b18473546a347e92f154f
Terry Robinson <tdrobinson94@comcast.net>
2020-04-24 00:06:12 -0400

    destroy event updated

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -39,8 +39,8 @@ class EventController {
 
     input.user_id = auth.user.id
     console.log(input.user_id)
-    // const event = await Event.findBy('id', input.id)
-    const event = await Event.query().where('id', input.id)
+    const event = await Event.findBy('id', input.id)
+    // const event = await Event.query().where('id', input.id)
 
     await event.delete()
 
