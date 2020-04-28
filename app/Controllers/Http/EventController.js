71620531dc9726ba0c2fc37888d039d3b594d998
Terry Robinson <tdrobinson94@comcast.net>
2020-04-28 19:50:22 -0400

    adding events

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -22,10 +22,10 @@ class EventController {
   }
 
   async store_multiple({ auth, request, response }) {
-    const input = request.only('item_type', 'title', 'frequency', 'description', 'start_date', 'end_date', 'start_time', 'end_time', 'location')
+    const input = request.only(['item_type', 'title', 'frequency', 'description', 'start_date', 'end_date', 'start_time', 'end_time', 'location'])
 
     input.user_id = auth.user.id
-    // input.group_id += 1
+    input.group_id += 1
 
     const newEvent = await Event.createMany(input)
 
