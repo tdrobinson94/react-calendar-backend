ce6467f8d018f9ce6c1e43e669a328f8dff02458
Terry Robinson <tdrobinson94@comcast.net>
2020-04-28 20:52:13 -0400

    adding events

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -25,11 +25,11 @@ class EventController {
     const input = request.collect(['item_type', 'title', 'frequency', 'description', 'start_date', 'end_date', 'start_time', 'end_time', 'location'])
 
     input.user_id = auth.user.id
-    // input.group_id += 1
+    input.group_id += 1
 
     const newEvent = await Event.createMany(input)
 
-    return response.json(newEvent)
+    return response.json(newEvent.toJSON())
   }
 
   async show({ request, response }) {
