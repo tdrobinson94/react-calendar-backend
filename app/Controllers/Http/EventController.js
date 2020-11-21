efd652dc15d35c1051e7de7468d3b3f7ff573c7d
tdrobinson94 <tdrobinson94@gmail.com>
2020-11-21 13:51:12 -0500

    group id

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -91,9 +91,9 @@ class EventController {
   }
 
   async destroyGroup({ request, response }) {
-    const input = request.only('id', 'group_id')
+    const input = request.only('group_id')
 
-    const events = await Event.query().where('group_id', '' + input.group_id + '').fetch()
+    const events = await Event.query().where('group_id', '' + input.group_id + '')
 
     await events.delete()
 
