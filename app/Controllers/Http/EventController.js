868623959c4ea75a558dc0cc23090f4922eb4630
tdrobinson94 <tdrobinson94@gmail.com>
2021-03-08 10:41:38 -0500

    deleting groups

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -113,16 +113,11 @@ class EventController {
 
     const events = await Event.query().where('group_id', input.groupId).fetch();
 
-    events.forEach((event) => {
-      await event;
+    for (let i = 0; i >= events.length; i++) {
+      const event = await Event.findBy('id', events[i].id)
 
-      event.delete()
-    })
-    // for (let i = 0; i >= events.length; i++) {
-    //   const event = await Event.findBy('id', events.id)
-
-    //   await event.delete();
-    // }
+      await event.delete();
+    }
 
     response.json({
       message: 'Group was successfully deleted',
