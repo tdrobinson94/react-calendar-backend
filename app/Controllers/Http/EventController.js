1aa63a1b09f9faf91dbfd48b63bb13a9ba308f4c
tdrobinson94 <tdrobinson94@gmail.com>
2021-03-08 00:47:39 -0500

    leap year

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -29,6 +29,8 @@ class EventController {
       if (forecast_date.format('Y') % 4 == 0 && forecast_date.format('YYYY-MM-DD') !== startDate && inputFrequency === '366') {
         inputFrequency = (Number(input.frequency) + 1).toString();
         date = forecast_date.format('Y') + '-' + forecast_date.format('MM') + '-' + (Number(forecast_date.format('DD')) + 1).toString();
+      } else if (inputFrequency === '30') {
+        inputFrequency = forecast_date.daysInMonth();
       } else {
         inputFrequency = input.frequency;
         date = forecast_date.format('YYYY-MM-DD')
